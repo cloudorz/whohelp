b98a9cff7b1c3e5cfcd0ea8e709c3504ea6b18ec
cloud <cloudcry@gmail.com>
2011-09-10 16:33:19 +0800

    Initial Commit

---
+++ WhoHelp/WhoHelpAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  WhoHelpAppDelegate.h
+//  WhoHelp
+//
+//  Created by cloud on 11-9-10.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WhoHelpAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ WhoHelpTests/WhoHelpTests.h
@@ -0,0 +1,13 @@
+//
+//  WhoHelpTests.h
+//  WhoHelpTests
+//
+//  Created by cloud on 11-9-10.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WhoHelpTests : SenTestCase
+
+@end
